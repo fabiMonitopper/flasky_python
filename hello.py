6d53c73d2ce446a56151dd3693282360e8a5126d
fvsouza <fv_souza@yahoo.com.br>
2022-04-18 20:05:27 -0300

    segundo exemplo

--- hello.py
+++ hello.py
@@ -7,3 +7,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1> Hello Word </h1>'
+
+@app.route('/user/<name>')
+def user(name):
+    return'<h1>Hello, {}!</h1>'.format(name)
+    
